6a8e2dd621c4edeb0c8bd04647cc899af6fc3e09
asnganje <abd.nganje@gmail.com>
2023-12-29 15:02:11 +0300

    Add nodemon for server auto restart

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const express = require('express')
 
 // call the express function
 const app = express()
+
+// create port, routes and let app listen to that port
+const port = 3005
+// create routes using get
+app.get('/', (req, res)=> {
+    return res.send('Home page is here')
+})
+
+app.get('/login', (req,res)=> {
+    return res.send('This is the login page')
+})
+app.listen(port, ()=> {
+    console.log(`App is listening on port ${port}`);
+})
